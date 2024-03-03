147be64e25a85a48a92050a9e5e57387f49f8e40
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-03-03 16:15:49 +0400

    fix resolving live template xml

--- src/main/java/org/antlr/intellij/plugin/templates/ANTLRLiveTemplatesProvider.java
+++ src/main/java/org/antlr/intellij/plugin/templates/ANTLRLiveTemplatesProvider.java
@@ -2,14 +2,14 @@ package org.antlr.intellij.plugin.templates;
 
 import consulo.annotation.component.ExtensionImpl;
 import consulo.language.editor.template.DefaultLiveTemplatesProvider;
-import org.jetbrains.annotations.Nullable;
+import jakarta.annotation.Nonnull;
 
 @ExtensionImpl
 public class ANTLRLiveTemplatesProvider implements DefaultLiveTemplatesProvider
 {
-	// make sure module shows liveTemplates as source dir or whatever dir holds "lexer"
-	public static final String[] TEMPLATES = {"lexer/user"};
+	public static final String[] TEMPLATES = {"/org/antlr/intellij/plugin/templates/user.xml"};
 
+	@Nonnull
 	@Override
 	public String[] getDefaultLiveTemplateFiles() {
 		return TEMPLATES;
