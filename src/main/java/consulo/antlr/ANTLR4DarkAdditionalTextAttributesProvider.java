cc086f152753968da50dd3814ac9045b53883c60
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-03-19 16:53:33 +0400

    migrate to new color scheme api

--- src/main/java/consulo/antlr/ANTLR4DarkAdditionalTextAttributesProvider.java
+++ src/main/java/consulo/antlr/ANTLR4DarkAdditionalTextAttributesProvider.java
@@ -1,28 +1,29 @@
 package consulo.antlr;
 
 import consulo.annotation.component.ExtensionImpl;
-import consulo.colorScheme.AdditionalTextAttributesProvider;
+import consulo.colorScheme.AttributesFlyweightBuilder;
+import consulo.colorScheme.EditorColorSchemeExtender;
 import consulo.colorScheme.EditorColorsScheme;
+import consulo.ui.color.RGBColor;
 import jakarta.annotation.Nonnull;
+import org.antlr.intellij.plugin.ANTLRv4SyntaxHighlighter;
 
 /**
  * @author VISTALL
  * @since 20.05.2024
  */
 @ExtensionImpl
-public class ANTLR4DarkAdditionalTextAttributesProvider implements AdditionalTextAttributesProvider
-{
-	@Nonnull
-	@Override
-	public String getColorSchemeName()
-	{
-		return EditorColorsScheme.DARCULA_SCHEME_NAME;
-	}
+public class ANTLR4DarkAdditionalTextAttributesProvider implements EditorColorSchemeExtender {
+    @Nonnull
+    @Override
+    public String getColorSchemeId() {
+        return EditorColorsScheme.DARCULA_SCHEME_NAME;
+    }
 
-	@Nonnull
-	@Override
-	public String getColorSchemeFile()
-	{
-		return "/consulo/antlr/ANTLRv4Darcula.xml";
-	}
+    @Override
+    public void extend(Builder builder) {
+        builder.add(ANTLRv4SyntaxHighlighter.RULENAME, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0xFF, 0xC6, 0x6D))
+            .build());
+    }
 }
--- src/main/java/consulo/antlr/ANTLR4LightAdditionalTextAttributesProvider.java
+++ src/main/java/consulo/antlr/ANTLR4LightAdditionalTextAttributesProvider.java
@@ -1,28 +1,29 @@
 package consulo.antlr;
 
 import consulo.annotation.component.ExtensionImpl;
-import consulo.colorScheme.AdditionalTextAttributesProvider;
+import consulo.colorScheme.AttributesFlyweightBuilder;
+import consulo.colorScheme.EditorColorSchemeExtender;
 import consulo.colorScheme.EditorColorsScheme;
+import consulo.ui.color.RGBColor;
 import jakarta.annotation.Nonnull;
+import org.antlr.intellij.plugin.ANTLRv4SyntaxHighlighter;
 
 /**
  * @author VISTALL
  * @since 20.05.2024
  */
 @ExtensionImpl
-public class ANTLR4LightAdditionalTextAttributesProvider implements AdditionalTextAttributesProvider
-{
-	@Nonnull
-	@Override
-	public String getColorSchemeName()
-	{
-		return EditorColorsScheme.DEFAULT_SCHEME_NAME;
-	}
+public class ANTLR4LightAdditionalTextAttributesProvider implements EditorColorSchemeExtender {
+    @Override
+    public void extend(Builder builder) {
+        builder.add(ANTLRv4SyntaxHighlighter.RULENAME, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0x47, 0x47, 0x8E))
+            .build());
+    }
 
-	@Nonnull
-	@Override
-	public String getColorSchemeFile()
-	{
-		return "/consulo/antlr/ANTLRv4Default.xml";
-	}
+    @Nonnull
+    @Override
+    public String getColorSchemeId() {
+        return EditorColorsScheme.DEFAULT_SCHEME_NAME;
+    }
 }
